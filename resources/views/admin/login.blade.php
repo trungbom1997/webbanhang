4ad734d097b954c68b37578793540221aa60d8a7
trungbom <trungbom1997@gmail.com>
2021-03-03 17:25:08 +0700

    test2

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -39,6 +39,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 				<div class="clearfix"></div>
 				<input type="submit" value="Sign In" name="login">
 		</form>
+		
 		<p>Don't Have an Account ?<a href="registration.html">Create an account</a></p>
 </div>
 </div>
